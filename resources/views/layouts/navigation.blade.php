5ac76adea148707dd5c0c4d31e0ddb9f78e538d3
anikievgr <anikievgr.2005@gmail.com>
2023-11-11 17:05:07 +0300

    меняет цветовую схему у страницы пользователя

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white  mainNavigation">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('tasks')" :active="request()->routeIs('tasks')">
                         {{ __('Задачи') }}
                     </x-nav-link>
+                    <x-nav-link >
+                        {{ __('Привет') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -23,7 +26,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div class="mainTextColorH">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -46,16 +49,6 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
             </div>
         </div>
     </div>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,14 +4,9 @@
             {{ __('Задачи') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+    <!--Блок с тасками-->
+    <div class="mainBlock borderOne">
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo modi nam nesciunt natus doloremque, aspernatur magnam quasi qui neque odio assumenda cumque beatae ullam voluptatum explicabo fugiat mollitia, perspiciatis ea.
                     You're logged in!
-                </div>
-            </div>
-        </div>
     </div>
 </x-app-layout>
